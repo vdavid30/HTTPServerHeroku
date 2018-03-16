9c6ee029e3884b48143a182b6da3fe1eb484600d
David Vaca <david.vaca@mail.escuelaing.edu.co>
2018-03-16 09:52:44 -0500

    Heroku 2

--- src/main/java/com/mycompany/httpserver/HttpServer.java
+++ src/main/java/com/mycompany/httpserver/HttpServer.java
@@ -4,6 +4,7 @@ package com.mycompany.httpserver;
 import java.net.*;
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 public class HttpServer implements Runnable{
@@ -16,7 +17,9 @@ public class HttpServer implements Runnable{
     public void run() {
         try {     
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String inputLine, outputLine, format, data;
+            String inputLine, format, data;
+            String outputLine = "";
+            boolean isImage = false;
             byte[] bytes;
             inputLine = in.readLine();            
             if(inputLine != null){
@@ -26,9 +29,11 @@ public class HttpServer implements Runnable{
                         data = "" + bytes.length;
                         format = "text/html";                   
                     }else if(inputLine.contains(".jpg")){
-                        bytes = Files.readAllBytes(new File("./"+inputLine).toPath());
+                        File img = new File("./"+inputLine);
+                        bytes = Files.readAllBytes(img.toPath());
                         data = "" + bytes.length;
-                        format = "image/html";                                   
+                        format = "image/html";   
+                        isImage = true;
                     }else{
                         bytes = Files.readAllBytes(new File("./index.html").toPath());
                         data = "" + bytes.length;
@@ -40,10 +45,18 @@ public class HttpServer implements Runnable{
                 format = "text/html";                   
 
             }
-            outputLine = "HTTP/1.1 200 OK\r\n" 
+            if(isImage){
+                outputLine = "HTTP/1.1 200 OK\r\n" 
                                 + "Content-Type: " + format + "\r\n"
                                 + "Content-Length: " + data
                                 + "\r\n\r\n";   
+            }else if(!isImage){
+                outputLine = "HTTP/1.1 200 OK\r\n" 
+                                + "Content-Type: " + format + "\r\n"
+                                + "Content-Length: " + data
+                                +"<img src="+bytes.toString()+"/>"
+                                + "\r\n\r\n";   
+            }
             socket.getOutputStream().write(getHeader(outputLine, bytes));
             socket.close();
         } catch (IOException ex) {
