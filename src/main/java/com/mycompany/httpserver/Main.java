ed469c2d97155e98cc90c1040a1f97b9ac7586ae
David Vaca <david.vaca@mail.escuelaing.edu.co>
2018-03-16 09:21:59 -0500

    Prueba 2 heroku

--- src/main/java/com/mycompany/httpserver/Main.java
+++ src/main/java/com/mycompany/httpserver/Main.java
@@ -5,10 +5,21 @@
  */
 package com.mycompany.httpserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  *
  * @author 2114928
  */
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(new Integer(System.getenv("PORT")));
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        while (true){
+            executor.execute(new HttpServer(serverSocket.accept()));           
+        }
+    }
 }
