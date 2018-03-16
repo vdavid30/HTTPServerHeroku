89281a84a527081499f5fefac81ebc5c71b8a161
unknown <2114928@labinfo.is.escuelaing.edu.co>
2018-03-16 09:15:08 -0500

    Funcionalidad concurrente agregada y prueba en Heroku

--- src/main/java/com/mycompany/httpserver/HttpServer.java
+++ src/main/java/com/mycompany/httpserver/HttpServer.java
@@ -3,53 +3,68 @@ package com.mycompany.httpserver;
 
 import java.net.*;
 import java.io.*;
-public class HttpServer {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = null;
-        serverSocket = new ServerSocket(new Integer(System.getenv("PORT")));
-        Socket clientSocket = null; 
-        clientSocket = serverSocket.accept();       
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String inputLine, outputLine;
-        while ((inputLine = in.readLine()) != null) {
-            //System.out.println("Received: " + "ENTRO AL WHILE ");
-            if (!in.ready()) {
-                System.out.println("NO ESTA LISTO  ");
-            }else{
-                System.out.println("ESTA LISTO");
-            }
-        }
-        outputLine = "HTTP/1.1 200 OK \n"
-            +"Content-Type: text/html; charset=utf-8\n"
-            +"Cache-Control: public, max-age=60, s-maxage=300\n"
-            +"Vary: Accept-Encoding\n"
-            +"Content-Encoding: raw\n"
-            +"Server: DPS/1.3.5\n"
-            +"X-SiteId: 2000\n"
-            +"Set-Cookie: dps_site_id=2000; path=/\n"
-            +"ETag: a1be084ec13bc207c17c49955f875fab\n"
-            +"Date: Mon, 26 Feb 2018 21:47:54 GMT\n"
-            +"Connection: keep-alive\n"
-            +"Transfer-Encoding: chunked\n" 
-            +"\n"
-            +"3c6f"
-            + "<!DOCTYPE html>"
-            + "<html>"
-            + "<head>"
-            + "<meta charset=\"UTF-8\">"
-            + "<title>Title of the document</title>\n"
-            + "</head>"
-            + "<body>"
-            + "My Web Site"
-            + "</body>"
-            + "</html>"                                    
-            + inputLine;
-        out.println(outputLine);
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class HttpServer implements Runnable{
+    private Socket socket;
+    public HttpServer(Socket socket){
+       this.socket = socket; 
+    }
 
-        out.close();
-        in.close();
-        clientSocket.close();
-        serverSocket.close();
+    @Override
+    public void run() {
+        try {     
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String inputLine, outputLine, format, data;
+            byte[] bytes;
+            inputLine = in.readLine();            
+            if(inputLine != null){
+                    inputLine = inputLine.split(" ")[1];
+                    if(inputLine.contains(".html")){
+                        bytes = Files.readAllBytes(new File("./"+inputLine).toPath());
+                        data = "" + bytes.length;
+                        format = "text/html";                   
+                    }else if(inputLine.contains(".jpg")){
+                        bytes = Files.readAllBytes(new File("./"+inputLine).toPath());
+                        data = "" + bytes.length;
+                        format = "image/html";                                   
+                    }else{
+                        bytes = Files.readAllBytes(new File("./index.html").toPath());
+                        data = "" + bytes.length;
+                        format = "text/html";                   
+                    }
+            }else{
+                bytes = Files.readAllBytes(new File("./index.html").toPath());
+                data = "" + bytes.length;
+                format = "text/html";                   
+
+            }
+            outputLine = "HTTP/1.1 200 OK\r\n" 
+                                + "Content-Type: " + format + "\r\n"
+                                + "Content-Length: " + data
+                                + "\r\n\r\n";
+            out.println(outputLine);
+
+            out.close();
+            in.close();
+            socket.getOutputStream().write(getHeader(outputLine, bytes));
+            socket.close();
+        } catch (IOException ex) {
+                Logger.getLogger(HttpServer.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    }
+    
+    public byte[] getHeader(String outputLine, byte [] bytes){
+        byte [] bytesEn = outputLine.getBytes();
+        byte[] answ = new byte[bytes.length + bytesEn.length];
+        for (int i = 0; i < bytesEn.length; i++) {
+            answ[i] = bytesEn[i];
+        }
+        for (int i = bytesEn.length; i < bytesEn.length + bytes.length; i++) {
+            answ[i] = bytes[i - bytesEn.length];
+        } 
+        return answ;
     }
 }
